2bb1182839f9cb2ccbbe7634fad2bc4839932794
Michael Smith <me@michaelsmith.xyz>
2025-08-20 20:51:06 -0400

    add neogit

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -132,6 +132,19 @@ return {
     },
   },
   {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+    },
+    config = function()
+      local neogit = require('neogit')
+      neogit.setup({
+        graph_style = 'unicode',
+      })
+    end,
+  },
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'williamboman/mason.nvim', opts = {} },
